4784d70fb8c7b655461f049693036cc9e9b7a20d
jk <jkwak1635@gmail.com>
2024-02-11 01:20:06 +0900

    chore: eslint, prettier 세팅 및 절대 경로 설정

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: { jsx: true },
+        jsx: true,
+        useJSXTextNode: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier',
+        'plugin:import/recommended',
+    ],
+    plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks'],
+    settings: { 'import/resolver': { typescript: {} } },
+    rules: {
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+
+        'react/prop-types': 'off',
+
+        'import/newline-after-import': 'warn',
+
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-unresolved': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+                pathGroups: [
+                    {
+                        pattern: '{react,react-dom}',
+                        group: 'builtin',
+                        position: 'before',
+                    },
+                ],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+    },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: "avoid",
+    trailingComma: "es5",
+    printWidth: 120,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+};
