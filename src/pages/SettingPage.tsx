b1045ef8645dd91900c06f5c42845b78e7be5d35
jk <jkwak1635@gmail.com>
2024-02-11 02:06:59 +0900

    feat: 게임 설정 페이지 기본 마크업 추가

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SettingPage = () => {
-    return <div>SettingPage</div>;
+    return (
+        <div>
+            <div>
+                <label>보드 사이즈: </label>
+                <select>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                </select>
+            </div>
+            <div>
+                <label>승리 조건: </label>
+                <select>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                </select>
+            </div>
+            <div>
+                <label>Player 1 마크 모양: </label>
+                <select>
+                    <option>X</option>
+                    <option>O</option>
+                    <option>세모</option>
+                    <option>체크</option>
+                </select>
+            </div>
+            <div>
+                <label>Player 2 마크 모양: </label>
+                <select>
+                    <option>O</option>
+                    <option>X</option>
+                    <option>세모</option>
+                    <option>체크</option>
+                </select>
+            </div>
+            <div>
+                <label>마크 색상: </label>
+                <select>
+                    <option>red</option>
+                    <option>yellow</option>
+                    <option>blue</option>
+                </select>
+            </div>
+            <div>
+                <label>스타팅 멤버: </label>
+                <select>
+                    <option>랜덤</option>
+                    <option>Player 1</option>
+                    <option>Player 2</option>
+                </select>
+            </div>
+            <Link to="/board">GO</Link>
+        </div>
+    );
 };
 
 export default SettingPage;
