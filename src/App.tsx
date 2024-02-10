484dfc2d9a77c2dc007385fa66b9856352522920
jk <jkwak1635@gmail.com>
2024-02-11 01:30:22 +0900

    feat: router 및 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import BoardPage from './pages/BoardPage'
+import HistoryPage from './pages/HistoryPage'
+import HomePage from './pages/HomePage'
+import SettingPage from './pages/SettingPage'
 
 function App() {
     return (
         <div>
-            <h1>tic tac toe</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/setting" element={<SettingPage />} />
+                    <Route path="/board" element={<BoardPage />} />
+                    <Route path="/history" element={<HistoryPage />} />
+                </Routes>
+            </BrowserRouter>{' '}
         </div>
     )
 }
