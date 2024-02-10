7f7918d5dc65a88a6b93b0e11afd1fbcd6571f5b
jk <jkwak1635@gmail.com>
2024-02-11 01:50:44 +0900

    feat: 홈 화면 버튼 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-    return <div>HomePage</div>;
+    return (
+        <div>
+            <Link to="/setting">게임 시작</Link>
+            <Link to="/history">기록된 게임 보기</Link>
+            {/*TODO: 배포 전 삭제*/}
+            <Link to="/board">게임 화면</Link>
+        </div>
+    );
 };
 
 export default HomePage;
