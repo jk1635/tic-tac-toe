2ec54bd0493108ffd68cc1697130fdea78b79b06
jk <jkwak1635@gmail.com>
2024-02-16 17:46:08 +0900

    feat: Home 스타일 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
+import styled from '@emotion/styled';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from 'components/Button';
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
-            <Link to="/setting">게임 시작</Link>
-            <Link to="/history">저장된 게임</Link>
+            <ButtonWrapper>
+                <Button onClick={() => navigate('/setting')}>start</Button>
+                <Button onClick={() => navigate('/history')}>history</Button>
+            </ButtonWrapper>
         </div>
     );
 };
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
 
 export default HomePage;
